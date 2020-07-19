cf87b626d351d3ab18595dcbdbd162c482b6c9b7
Narongrit Jaijan <60143431@g.cmru.ac.th>
2020-07-19 14:42:25 +0700

    bla

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        $posts = Post::paginate(5);
+        $data =[
+            'posts' => $posts
+        ];
+        return view('home', $data);
     }
-    public function contact(){
-        return view('contact');
+    public function create(){
+        $cagories = Category::all();
+        $data = [
+            'categories' => $cagories
+        ];
+        return view('create',$data);
     }
-    public function about(){
-        return view('about');
+    public function login(){
+        return view('login');
+    }
+    public function store(Request $request){
+        //$name = $POST['name];
+                $detail = $request->input('detail');
+                $category_id =$request->input('category_id');
+
+                $post = new Post();
+                $post->user_id =1;
+                $post->status =0;
+                $post->detail = $detail;
+                $post->category_id =$category_id;
+                $post->save();
+
+                return redirect('/');
+    }
+    public function delete($id){
+        if($id ==""){
+            return redirect('/');
+        }
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
-Route::get('/contact', 'HomeController@contact');
-Route::get('/about', 'HomeController@about');
+Route::get('/create', 'HomeController@create');
+Route::get('/login', 'HomeController@login');
+Route::post('/store', 'HomeController@store');
+Route::get('/delete/{id}', 'HomeController@delete');
+
+
+
+
+
 //Route::get('/', 'HomeController@index');
 // Route::get('/', 'HomeController@show');
 // Route::get('/', 'HomeController@create');
